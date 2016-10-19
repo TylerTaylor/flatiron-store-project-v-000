720d022533b37f1f61ade071be190ed0a851832f
tylertaylor <vikingtyty@gmail.com>
2016-10-18 21:41:37 -0500

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,6 +2,7 @@ class CartsController < ApplicationController
 
   def show
     @cart = Cart.find(params[:id])
+    @user = current_user
   end
 
   def checkout
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,6 +2,7 @@ class LineItemsController < ApplicationController
 
   def create
     current_user.create_current_cart unless current_user.current_cart
+    
     line_item = current_user.current_cart.add_item(params[:item_id])
     if line_item.save
       redirect_to cart_path(current_user.current_cart), {notice: 'Item added to cart'}
